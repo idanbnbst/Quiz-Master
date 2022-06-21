235ebc835d6ba67ccb811e2776aa9cedbad1c801
idanbnbst <idanbnbst@gmail.com>
2022-06-21 12:31:45 +0300

    adding button click functionallity

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionLabel;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButton;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    int correctAnswerIndex;
 
     void Start()
     {
@@ -16,6 +20,25 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonLabel = answerButton[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonLabel.text = question.GetAnswer(i);
+        }
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionLabel.text = "Correct!";
+            buttonImage = answerButton[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            string correctAnswerLabel = question.GetAnswer(correctAnswerIndex);
+            questionLabel.text = "Wrong. The correct answer is " + correctAnswerLabel;
+            buttonImage = answerButton[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 }
